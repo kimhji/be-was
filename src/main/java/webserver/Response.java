720f02aac375ce778ac713d13d1122d752327524
kimhji <twin4185@naver.com>
2026-01-02 21:06:04 +0900

    fix[3단계]: 불필요한 checked Exception 삭제

--- src/main/java/webserver/Response.java
+++ src/main/java/webserver/Response.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 
 public class Response {
-    static byte[] processReq(SimpleReq simpleReq) throws Exception{
+    static byte[] processReq(SimpleReq simpleReq){
         byte[] result = "".getBytes();
         switch(simpleReq.method){
             case GET:
@@ -16,15 +16,20 @@ public class Response {
         return result;
     }
 
-    static private byte[] getStaticSources(String path) throws Exception{
+    static private byte[] getStaticSources(String path){
         if(path.compareTo("/")==0) return "<h1>Hello World</h1>".getBytes();
 
         String wholePath = "./src/main/resources/static"+path;
         File file = new File(wholePath);
-        if(!file.exists()) throw new Exception("해당 static 파일이 존재하지 않습니다.");
-        FileInputStream fr = new FileInputStream(wholePath);
+        if(!file.exists()) throw new RuntimeException("해당 static 파일이 존재하지 않습니다.");
+        try {
+            FileInputStream fr = new FileInputStream(wholePath);
 
-        byte[] buffer = fr.readAllBytes();
-        return buffer;
+            byte[] buffer = fr.readAllBytes();
+            return buffer;
+        }
+        catch (Exception e) {
+            throw new RuntimeException("파일을 읽는 과정 중에 예상치 못한 에러가 발생했습니다.");
+        }
     }
 }
--- src/main/java/webserver/SimpleReq.java
+++ src/main/java/webserver/SimpleReq.java
@@ -16,12 +16,12 @@ class SimpleReq{
     Method method;
     String path;
     Map<String, String> queryParam = new HashMap<>();
-    SimpleReq(String req) throws Exception{
-        if(req == null || req.isBlank()) throw new Exception("요청이 비어있습니다.");
+    SimpleReq(String req){
+        if(req == null || req.isBlank()) throw new RuntimeException("요청이 비어있습니다.");
         String[] lines = req.split("\n");
         String[] firstHeader = lines[0].trim().split(" ");
         if(firstHeader.length < 3){
-            throw new Exception("헤더의 첫 줄이 예상하지 못한 형식으로 들어왔습니다.");
+            throw new RuntimeException("헤더의 첫 줄이 예상하지 못한 형식으로 들어왔습니다.");
         }
         method = getMethod(firstHeader[0]);
         path = firstHeader[1];
@@ -34,7 +34,7 @@ class SimpleReq{
             }
             path = pathSplit[0];
         }
-        if(method == Method.NONE) throw new Exception("알맞지 않은 method입니다.");
+        if(method == Method.NONE) throw new RuntimeException("알맞지 않은 method입니다.");
     }
     SimpleReq(Method method, String path) {
         this.method = method;
