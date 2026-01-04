4e2a1e554e59fd084e1eb8859bae0435dfd10968
kimhji <twin4185@naver.com>
2026-01-05 04:09:20 +0900

    fix[3단계]: 근거있는 thread Pool 갯수 지정. - 실제 OS에서 사용하여 JVM이 파악하는 논리적 cpu 코어 수를 기반으로 thread pool을 생성할 수 있도록 수정했습니다.

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -17,10 +17,15 @@ public class WebServer {
 
 
     public static void main(String args[]) throws Exception {
-        executor = new ThreadPoolExecutor(
-                2, 4, 60,
-                TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(10)
+        int cpuCount = Runtime.getRuntime().availableProcessors();
+        logger.info("Available processors: {}", cpuCount);
+
+        ExecutorService executor = new ThreadPoolExecutor(
+                cpuCount,
+                cpuCount,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>()
         );
 
         int port = 0;
