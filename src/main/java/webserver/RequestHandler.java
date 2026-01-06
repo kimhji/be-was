54768d00fc8dca7d64b7bd59d869fe26ba9bd215
kimhji <twin4185@naver.com>
2026-01-06 15:30:17 +0900

    refactor: ContentType enum 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -25,17 +25,17 @@ public class RequestHandler implements Runnable {
                     SimpleReq realReq = new SimpleReq(SimpleReq.Method.GET, "/registration/index.html");
                     byte[] body = StaticFileProcessor.processReq(realReq);
                     if (body == null) throw WebStatusConverter.inexistenceStaticFile();
-                    return new Response(WebException.HTTPStatus.OK, body, contentType(realReq.path));
+                    return new Response(WebException.HTTPStatus.OK, body, Response.contentType(realReq.path));
                 }
         );
         router.register(new SimpleReq(SimpleReq.Method.GET, "/create"), value -> {
             byte[] body = new User(value.queryParam.get("userId"), value.queryParam.get("password"), value.queryParam.get("name"), value.queryParam.get("email"))
                     .toString().getBytes();
-            return new Response(WebException.HTTPStatus.OK, body, "text/html;charset=utf-8");
+            return new Response(WebException.HTTPStatus.OK, body, Response.ContentType.HTML);
         });
 
         router.register(new SimpleReq(SimpleReq.Method.GET, "/"), dummy -> {
-            return new Response(WebException.HTTPStatus.OK, "<h1>Hello World</h1>".getBytes(), "text/html;charset=utf-8");
+            return new Response(WebException.HTTPStatus.OK, "<h1>Hello World</h1>".getBytes(), Response.ContentType.HTML);
 
         });
     }
@@ -53,27 +53,20 @@ public class RequestHandler implements Runnable {
                 if (simpleReq.method == SimpleReq.Method.GET) {
                     byte[] body = StaticFileProcessor.processReq(simpleReq);
                     if (body != null) {
-                        response = new Response(WebException.HTTPStatus.OK, body, contentType(simpleReq.path));
+                        response = new Response(WebException.HTTPStatus.OK, body, Response.contentType(simpleReq.path));
                     }
                 }
                 if (response == null) {
                     response = router.route(simpleReq);
                 }
 
-                responseHeaderByStatusAndType(dos, response.body.length, response.statusCode, response.contentType);
+                response.setResponseHeader(dos);
 
                 responseBody(dos, response.body);
             } catch (WebException e) {
-                byte[] body = e.getMessage()
-                        .getBytes();
-
-                responseHeaderByStatusAndType(
-                        dos,
-                        body.length,
-                        e.statusCode,
-                        "text/plain; charset=utf-8"
-                );
-                responseBody(dos, body);
+                Response response = new Response(e.statusCode, e.getMessage().getBytes(), Response.ContentType.PLAIN_TEXT);
+                response.setResponseHeader(dos);
+                responseBody(dos, response.body);
             }
         } catch (IOException e) {
             logger.debug("Connection closed", e);
@@ -82,36 +75,8 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        responseHeaderByStatusAndType(dos, lengthOfBodyContent, WebException.HTTPStatus.OK, "text/html;charset=utf-8");
-    }
-
-    private void response200HeaderByType(DataOutputStream dos, int lengthOfBodyContent, SimpleReq simpleReq) {
-        responseHeaderByStatusAndType(dos, lengthOfBodyContent, WebException.HTTPStatus.OK, contentType(simpleReq.path));
-    }
-
-    private void responseHeaderByStatusAndType(DataOutputStream dos, int lengthOfBodyContent, WebException.HTTPStatus status, String contentType) {
-        try {
-            dos.writeBytes("HTTP/1.1 " + status.getHttpStatus() + " " + status.name() + " \r\n");
-            dos.writeBytes("Content-Type: " + contentType + "\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private static String contentType(String path) {
-        if (path.endsWith(".html")) return "text/html;charset=utf-8";
-        if (path.endsWith(".css")) return "text/css";
-        if (path.endsWith(".js")) return "application/javascript";
-        if (path.endsWith(".png")) return "image/png";
-        if (path.endsWith(".jpg") || path.endsWith(".jpeg")) return "image/jpeg";
-        if (path.endsWith(".svg")) return "image/svg+xml";
-        return "application/octet-stream";
-    }
-
     private void responseBody(DataOutputStream dos, byte[] body) throws IOException{
+        dos.writeBytes("\r\n");
         dos.write(body, 0, body.length);
         dos.flush();
     }
--- src/main/java/webserver/Response.java
+++ src/main/java/webserver/Response.java
@@ -2,16 +2,70 @@ package webserver;
 
 import customException.WebException;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class Response {
+    enum ContentType{
+        HTML("text/html;charset=utf-8"),
+        CSS("text/css"),
+        JS("application/javascript"),
+        PNG("image/png"),
+        JPG("image/png"),
+        JPEG("image/jpeg"),
+        SVG("image/svg+xml"),
+        OCTET("application/octet-stream"),
+        PLAIN_TEXT("text/plain; charset=utf-8")
+        ;
+
+        public String contentType;
+
+        ContentType(String contentType){
+            this.contentType = contentType;
+        }
+    }
     WebException.HTTPStatus statusCode;
-    String contentType;
+    ContentType contentType;
     byte[] body;
 
     public Response(WebException.HTTPStatus status,
                         byte[] body,
-                        String contentType) {
+                    ContentType contentType) {
         this.statusCode = status;
         this.body = body;
         this.contentType = contentType;
+    }
+
+
+
+    public void response200Header(DataOutputStream dos, int lengthOfBodyContent) throws IOException{
+        responseHeaderByStatusAndType(dos, lengthOfBodyContent, WebException.HTTPStatus.OK, ContentType.HTML);
+    }
+
+    public void response200HeaderByType(DataOutputStream dos, int lengthOfBodyContent, SimpleReq simpleReq) throws IOException{
+        responseHeaderByStatusAndType(dos, lengthOfBodyContent, WebException.HTTPStatus.OK, contentType(simpleReq.path));
+    }
+
+    public void responseHeaderByStatusAndType(DataOutputStream dos, int lengthOfBodyContent, WebException.HTTPStatus status, ContentType contentType) throws IOException{
+        dos.writeBytes("HTTP/1.1 " + status.getHttpStatus() + " " + status.name() + " \r\n");
+        dos.writeBytes("Content-Type: " + contentType.contentType + "\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+    }
+
+    public void setResponseHeader(DataOutputStream dos) throws IOException{
+        dos.writeBytes("HTTP/1.1 " + statusCode.getHttpStatus() + " " + statusCode.name() + " \r\n");
+        dos.writeBytes("Content-Type: " + contentType.contentType + "\r\n");
+        dos.writeBytes("Content-Length: " + (body!=null?body.length:0) + "\r\n");
+    }
+
+    public static ContentType contentType(String path) {
+        if (path.endsWith(".html")) return ContentType.HTML;
+        if (path.endsWith(".css")) return ContentType.CSS;
+        if (path.endsWith(".js")) return ContentType.JS;
+        if (path.endsWith(".png")) return ContentType.PNG;
+        if (path.endsWith(".jpg") || path.endsWith(".jpeg")) return ContentType.JPEG;
+        if (path.endsWith(".svg")) return ContentType.SVG;
+        if (path.endsWith(".txt")) return ContentType.PLAIN_TEXT;
+        return ContentType.OCTET;
     }
 }
