8f6cb48f22aa5a08efd4d90a592368de8fd88687
kimhji <twin4185@naver.com>
2026-01-07 20:51:09 +0900

    feat[5단계]: 회원 가입 페이지에 동일한 alert 기능 추가

--- src/main/resources/static/script/alert.js
+++ src/main/resources/static/script/alert.js
@@ -1,10 +1,26 @@
+async function formResponseProcess(response) {
+    if (response.status >= 400 && response.status < 500) {
+        const msg = await response.text();
+        alert("에러 발생: " + msg);
+        return;
+    }
+    if (response.status >= 300 && response.status < 400) {
+        window.location.href = response.headers.get("Location");
+        return;
+    }
 
-document.getElementById("login").addEventListener("submit", async (e) => {
-        e.preventDefault(); // 기본 submit 막기
-        const userId = document.querySelector("#userId").value;
-        const password = document.querySelector("#password").value;
+    // 성공
+    if (response.status >= 200 && response.status < 300) {
+        window.location.href = "/index.html";
+    }
+}
 
-        const response = await fetch("/user/login", {
+document.getElementById("login")?.addEventListener("submit", async (e) => {
+    e.preventDefault(); // 기본 submit 막기
+    const userId = document.querySelector("#userId").value;
+    const password = document.querySelector("#password").value;
+
+    const response = await fetch("/user/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
@@ -15,18 +31,28 @@ document.getElementById("login").addEventListener("submit", async (e) => {
         })
     });
 
-    if (response.status >= 400 && response.status < 500) {
-        const msg = await response.text();
-        alert("에러 발생: " + msg);
-        return;
-    }
-    if(response.status >= 300 && response < 400){
-        window.location.href = response.headers.get("Location");
-        return;
-    }
+    await formResponseProcess(response);
+});
 
-    // 성공
-    if(response.status >= 200 && response.status<300) {
-        window.location.href = "/index.html";
-    }
+document.getElementById("registration")?.addEventListener("submit", async (e) => {
+    e.preventDefault(); // 기본 submit 막기
+    const userId = document.querySelector("#userId").value;
+    const password = document.querySelector("#password").value;
+    const email = document.querySelector("#email").value;
+    const name = document.querySelector("#name").value;
+
+    const response = await fetch("/user/create", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            userId,
+            password,
+            email,
+            name
+        })
+    });
+
+    await formResponseProcess(response);
 });
