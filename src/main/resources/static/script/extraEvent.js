1630a9a82396467a3e1724507b596a1500075d59
kimhji <twin4185@naver.com>
2026-01-16 00:21:06 +0900

    feat[7단계]: 사용자 이미지 임시 수정 추가

--- src/main/resources/static/script/extraEvent.js
+++ src/main/resources/static/script/extraEvent.js
@@ -1,6 +1,6 @@
 function addListenerLikeAction(){
     const btn = document.getElementById('post__like__btn');
-
+    if(btn == null) return;
     btn.addEventListener('click', async () => {
         const postId = btn.dataset.postId;
         if (!postId) return;
@@ -18,5 +18,22 @@ function addListenerLikeAction(){
 
 }
 
+function addListenerSaveTempImage(){
+    const imageInput = document.getElementById('form_profile_image');
+    if(imageInput == null) return;
+
+    imageInput.addEventListener('change', async (e) => {
+        const file = e.target.files[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            document.getElementById('user-profile-preview').src = reader.result;
+        };
+        reader.readAsDataURL(file);
+    });
+}
+
 
 addListenerLikeAction();
+addListenerSaveTempImage();
